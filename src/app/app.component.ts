eb6ec1ce6f5f99726cf11cb2be36603cdb0fa49f
Arnab and Boni <arnabandboni@Arnabs-MacBook-Pro.local>
2018-11-11 11:01:27 +0530

    changes for ladder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent implements OnInit {
   title = 'app';
   index = 100;
   boardArray:Array<any> = [];
-  diceNumber:number;
+  diceNumber:number;  
   playerMarker1:number = 1;
   playerMarker2:number = 1;
   snackArray:Array<{boardNumber:number,destinationNum:number}> =[]
@@ -41,11 +41,66 @@ export class AppComponent implements OnInit {
       });
       --this.index;
     }
+
+    this.generateSnake();
   }
 
   generateSnake(){
-  
+    for(let i=0; i<=10;i++)
+    {
+      let _boardNumber = Math.floor(Math.random() * 100 + 20);
+      let  destNum = _boardNumber - (i +10);
+      
+      let _ladderNumber =  _boardNumber - 5;
+      let _ladderDest =  _boardNumber + (i + 1) 
+
+      if(_boardNumber < 100)
+      {
+        this.snackArray.push({
+          boardNumber:_boardNumber,
+          destinationNum:destNum
+        })
+        this.ladderArray.push({
+          boardNumber:_ladderNumber,
+          destinationNum:_ladderDest
+        })  
+
+      }
+    }
+
+    this.boardArray.forEach(element => {
+        this.snackArray.forEach( element2 => {
+          if(element.number === element2.boardNumber)
+          {
+           element.snake.flag = true;
+           element.snake.destination = element2.destinationNum;   
+          }
+        })
+
+        //ladder building
+        this.ladderArray.forEach( element2 => {
+          if(element.number === element2.boardNumber)
+          {
+           element.ladder.flag = true;
+           element.ladder.destination = element2.destinationNum;   
+          }
+        })
+
+        
+
+
+    });
+
+
+    console.log(this.snackArray);
+    console.log(this.boardArray);
   }
+
+  generateLadder(){
+
+  }
+
+
 
   getRandomInt(min, max) {
    console.log(Math.floor(Math.random() * max + min));
@@ -61,10 +116,49 @@ export class AppComponent implements OnInit {
     if(this.currentPlayer.toLowerCase() === 'player1')
     {
       this.playerMarker1 = this.playerMarker1 + this.diceNumber;
+      this.boardArray.forEach((o,i) => {
+        if(o.number == this.playerMarker1 && o.snake.flag == true)
+        {
+        
+          alert('snake detected');
+          this.playerMarker1 = o.snake.destination;
+          console.log(this.playerMarker1);
+        }
+
+        if(o.number == this.playerMarker1 && o.ladder.flag == true)
+        {
+        
+          alert('ladde detected');
+          this.playerMarker1 = o.ladder.destination;
+          console.log(this.playerMarker1);
+        }
+
+
+
+      })  
+      
+     
+      
     }
    else if(this.currentPlayer.toLowerCase() === 'player2')
    {
     this.playerMarker2 = this.playerMarker2 + this.diceNumber;
+    this.boardArray.forEach((o,i) => {
+      if(o.number == this.playerMarker2 && o.snake.flag == true)
+      {
+      
+        alert('snake detected');
+        this.playerMarker2 = o.snake.destination;
+      }
+
+      if(o.number == this.playerMarker2 && o.ladder.flag == true)
+      {
+      
+        alert('ladde detected');
+        this.playerMarker2 = o.ladder.destination;
+      }
+    }) 
+
    }
 
   }
